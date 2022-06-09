3fe366d063f5b177cd4dba184999948d7229d0c8
anilkumar.pentapati <anilkumar.pentapati@impressico.com>
2022-06-09 16:15:25 +0530

    Channel and flows are added

--- app/src/main/java/com/example/samplehiltretrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/samplehiltretrofit/ui/MainActivity.kt
@@ -8,53 +8,92 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.samplehiltretrofit.R
 import com.example.samplehiltretrofit.databinding.ActivityMainBinding
 import com.example.samplehiltretrofit.util.ResultResource
 import com.example.samplehiltretrofit.viewmodel.CharacterListViewModel
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    
-    private val characterListViewModel by viewModels<CharacterListViewModel> ()
-    private  val TAG = "MainActivity"
-    lateinit var mBinding:ActivityMainBinding
+
+    private val characterListViewModel by viewModels<CharacterListViewModel>()
+    private val TAG = "MainActivity"
+    lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding= ActivityMainBinding.inflate(layoutInflater)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-
         mBinding.fetchBtn.setOnClickListener {
             fetchData()
         }
-
     }
-    private fun fetchData(){
-        mBinding.pbDog.visibility= View.VISIBLE
+
+    private fun fetchData() {
+        mBinding.pbDog.visibility = View.VISIBLE
         characterListViewModel.fetchCharList()
-        characterListViewModel._response.observe(this, Observer { response->
-           mBinding.pbDog.visibility=View.GONE
-            when(response){
-               is ResultResource.SUCCESS ->{
-                   response.data?.let {
-                       Log.d(TAG, "fetchData: Success")
-                       Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
-                       Log.d(TAG, "fetchData: ${it.info.toString()}")
-                   }
-               }
-               is ResultResource.ERROR ->{
-                   response.message?.let {
-                       Log.d(TAG, "fetchData: Error "+it)
-                       Snackbar.make(mBinding.root,it,Snackbar.LENGTH_LONG).show()
-                   }
-               }
-               is ResultResource.LOADING->
-               {
-                   
-               }
-           }
+        observeEventData()
+    }
+
+    private fun observeEventData() {
+        lifecycleScope.launchWhenCreated {
+            characterListViewModel.eventFlow.collect { resultResource ->
+                when (resultResource) {
+                    is CharacterListViewModel.Event.Success -> {
+                        //mBinding.pbDog.visibility=View.GONE
+                        resultResource.characterList.let {
+                            Log.d(TAG, "observeEventData: Success")
+                            Log.d(TAG, "observeEventData: ")
+                            mBinding.txtResult.text =
+                                "Characters Count is ${resultResource.characterList.results.size}"
+                        }
+                    }
+                    is CharacterListViewModel.Event.Failed -> {
+                        resultResource.error.let {
+                            Snackbar.make(
+                                mBinding.root,
+                                resultResource.error.toString(),
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    is CharacterListViewModel.Event.Loading -> {
+                        if (resultResource.loading)
+                            mBinding.pbDog.visibility = View.VISIBLE
+                        else
+                            mBinding.pbDog.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeLiveData() {
+        characterListViewModel._response.observe(this, Observer { response ->
+            mBinding.pbDog.visibility = View.GONE
+            when (response) {
+                is ResultResource.SUCCESS -> {
+                    response.data?.let {
+                        Log.d(TAG, "fetchData: Success")
+                        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                        //Log.d(TAG, "fetchData: ${it.info.toString()}")
+                        Log.d(TAG, "fetchData: ${it.results.size}")
+                        mBinding.txtResult.text = "Characters Count is ${it.results.size}"
+                    }
+                }
+                is ResultResource.ERROR -> {
+                    response.message?.let {
+                        Log.d(TAG, "fetchData: Error " + it)
+                        Snackbar.make(mBinding.root, it, Snackbar.LENGTH_LONG).show()
+                    }
+                }
+                is ResultResource.LOADING -> {
+
+                }
+            }
         })
     }
 }
--- app/src/main/java/com/example/samplehiltretrofit/viewmodel/CharacterListViewModel.kt
+++ app/src/main/java/com/example/samplehiltretrofit/viewmodel/CharacterListViewModel.kt
@@ -2,26 +2,60 @@ package com.example.samplehiltretrofit.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.samplehiltretrofit.data.model.Character
 import com.example.samplehiltretrofit.data.model.CharacterList
 import com.example.samplehiltretrofit.data.repository.CharacterRepository
 import com.example.samplehiltretrofit.util.ResultResource
+import com.example.samplehiltretrofit.viewmodel.CharacterListViewModel.Event.Success
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import java.lang.Error
 import javax.inject.Inject
 
 @HiltViewModel
-class CharacterListViewModel @Inject constructor(private val characterRepository: CharacterRepository,
-                                                 application: Application
-):AndroidViewModel(application) {
+class CharacterListViewModel @Inject constructor(
+    private val characterRepository: CharacterRepository,
+    application: Application
+) : AndroidViewModel(application) {
+
     val _response: MutableLiveData<ResultResource<CharacterList>> = MutableLiveData()
-   // val response: LiveData<ResultResource<CharacterList>> = _response
-    fun fetchCharList()=viewModelScope.launch {
+    sealed class Event{
+        data class Success(val characterList: CharacterList):Event()
+        data class Loading(val loading:Boolean):Event()
+        data class Failed(val error:String?):Event()
+    }
+    private val eventChanel= Channel<Event>()
+    val eventFlow=eventChanel.receiveAsFlow()
+    // val response: LiveData<ResultResource<CharacterList>> = _response
+    fun fetchCharList() = viewModelScope.launch {
         characterRepository.getCharactersList().collect { values ->
-            _response.value = values
+         //   _response.value = values
+            values.data?.let{
+                when(values){
+                    is ResultResource.SUCCESS ->{
+                        with(eventChanel) {
+                            send(Event.Loading(false))
+                            send(Success(values.data))
+                        }
+                    }
+                    is ResultResource.LOADING ->{
+                        eventChanel.send(Event.Loading(true))
+                    }
+                    is ResultResource.ERROR ->{
+                        with(eventChanel){
+                            send(Event.Loading(false))
+                            send(Event.Failed(values.message))
+                        }
+
+                    }
+                }
+
+            }
+            //eventChanel.send(values.data?.let { Event.Success(it) })
         }
 
     }
